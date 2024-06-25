f3a92b69ce757117a49bf317106a980e119b279e
Marat Khusnetdinov <marat@khusnetdinov.ru>
2024-06-25 12:00:31 +0300

    Dispatch

--- crates/framework/examples/playground.rs
+++ crates/framework/examples/playground.rs
@@ -39,11 +39,10 @@ pub enum States {
     Dice,
 }
 
-async fn dispatch<STO: Storage<States> + Debug>(
-    storage: Arc<STO>,
+async fn dispatch(
+    storage: Arc<MemoryStorage<States>>,
     update: Update,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    println!("Storage: {:#?}", storage);
     match update.dispatch() {
         UpdateKind::Message(message) => match message.dispatch() {
             MessageKind::Text(text_message) => {
@@ -67,25 +66,18 @@ async fn dispatch<STO: Storage<States> + Debug>(
         UpdateKind::Unexpected(_) | _ => {}
     }
 
+    println!("Storage: {:#?}", storage);
+
     Ok(())
 }
-
-// pub struct Dispather<STO<STA>> {
-//     storage: STO<STA>
-// }
-
-// pub trait Dispatcher<STO<STA>> {
-//     fn dispatch();
-// }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let bots_api = BotsApi::from_env().await?;
     let storage = MemoryStorage::<States>::new();
-    let dispatcher = dispatch::<MemoryStorage<States>>;
 
     bots_api.commands(Commands::config()).await?;
-    bots_api.pooling(storage, dispatcher).await?;
+    bots_api.pooling(storage, dispatch).await?;
 
     Ok(())
 }
