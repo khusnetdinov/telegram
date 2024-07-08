9063a5283c881457a4dc8df3a1d5075793fbbc14
Marat Khusnetdinov <khusnetdinov@Mac-mini-Marat.local>
2024-07-08 21:23:56 +0300

    Remote: BusinessConnection

--- crates/framework/examples/macro.rs
+++ crates/framework/examples/macro.rs
@@ -1,15 +1,12 @@
-use telegram_bots_api::api::structs::webhook_info::WebhookInfo as Remote;
+use telegram_bots_api::api::structs::business_connection::BusinessConnection as Remote;
 use telegram_macros::FromRemote;
 
 #[derive(FromRemote)]
-pub struct WebhookInfo {
-    pub url: String,
-    pub has_custom_certificate: bool,
-    pub pending_update_count: i64,
-    pub ip_address: Option<String>,
-    pub last_error_date: Option<i64>,
-    pub last_error_message: Option<String>,
-    pub last_synchronization_error_date: Option<i64>,
-    pub max_connections: Option<u32>,
-    pub allowed_updates: Option<Vec<String>>,
+pub struct BusinessConnection {
+    pub id: String,
+    pub user: User,
+    pub user_chat_id: u64,
+    pub date: u64,
+    pub can_reply: bool,
+    pub is_enabled: bool,
 }
--- crates/framework/src/structs/update_kinds/business_connection.rs
+++ crates/framework/src/structs/update_kinds/business_connection.rs
@@ -1,7 +1,27 @@
-use telegram_bots_api::api::structs::business_connection::BusinessConnection as Inner;
-use telegram_macros::{DerefInner, FromInner};
+use crate::structs::user::User;
+use serde::{Deserialize, Serialize};
+use telegram_bots_api::api::structs::business_connection::BusinessConnection as Remote;
 
-#[derive(Debug, DerefInner, FromInner)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct BusinessConnection {
-    inner: Inner,
+    pub id: String,
+    pub user: User,
+    pub user_chat_id: u64,
+    pub date: u64,
+    pub can_reply: bool,
+    pub is_enabled: bool,
+}
+
+impl From<Remote> for BusinessConnection {
+    fn from(remote: Remote) -> Self {
+        Self {
+            id: remote.id,
+            // TODO: #[remote(into)]
+            user: remote.user.into(),
+            user_chat_id: remote.user_chat_id,
+            date: remote.date,
+            can_reply: remote.can_reply,
+            is_enabled: remote.is_enabled,
+        }
+    }
 }
