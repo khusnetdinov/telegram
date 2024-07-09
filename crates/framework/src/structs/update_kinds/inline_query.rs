ece833e67a1ea409b8729e2109618daae00ed036
Marat Khusnetdinov <marat@khusnetdinov.ru>
2024-07-09 21:32:46 +0300

    Remote: InlineQuery

--- crates/framework/src/structs/update_kinds/inline_query.rs
+++ crates/framework/src/structs/update_kinds/inline_query.rs
@@ -1,7 +1,30 @@
-use telegram_bots_api::api::structs::inline_query::InlineQuery as Inner;
-use telegram_macros::{DerefInner, FromInner};
+use crate::structs::message_kinds::location::Location;
+use crate::structs::user::User;
+use serde::{Deserialize, Serialize};
+use telegram_bots_api::api::structs::inline_query::InlineQuery as Remote;
 
-#[derive(Debug, Clone, DerefInner, FromInner)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct InlineQuery {
-    inner: Inner,
+    pub id: String,
+    pub from: User,
+    pub query: String,
+    pub offset: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub chat_type: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub location: Option<Location>,
+}
+impl From<Remote> for InlineQuery {
+    fn from(remote: Remote) -> Self {
+        Self {
+            id: remote.id,
+            // TODO: #[remote(into)]
+            from: remote.from.into(),
+            query: remote.query,
+            offset: remote.offset,
+            chat_type: remote.chat_type,
+            // TODO: #[remote(option)]
+            location: remote.location.map(|inner| inner.into()),
+        }
+    }
 }
