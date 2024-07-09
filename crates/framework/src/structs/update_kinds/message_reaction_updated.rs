c4dedf303a6501fb8158c81de0398829f8e40725
Marat Khusnetdinov <marat@khusnetdinov.ru>
2024-07-09 23:32:20 +0300

    Remote: MessageReactionUpdated

--- crates/framework/src/structs/update_kinds/message_reaction_updated.rs
+++ crates/framework/src/structs/update_kinds/message_reaction_updated.rs
@@ -1,7 +1,46 @@
-use telegram_bots_api::api::structs::message_reaction_updated::MessageReactionUpdated as Inner;
-use telegram_macros::{DerefInner, FromInner};
+use crate::enums::reaction_type::ReactionType;
+use crate::structs::chat::Chat;
+use crate::structs::message_id::MessageId;
+use crate::structs::user::User;
+use serde::{Deserialize, Serialize};
+use telegram_bots_api::api::structs::message_reaction_updated::MessageReactionUpdated as Remote;
 
-#[derive(Debug, Clone, DerefInner, FromInner)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub struct MessageReactionUpdated {
-    inner: Inner,
+    pub chat: Chat,
+    pub message_id: MessageId,
+    pub date: i64,
+    pub old_reaction: Vec<ReactionType>,
+    pub new_reaction: Vec<ReactionType>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub user: Option<User>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub actor_chat: Option<Chat>,
+}
+impl From<Remote> for MessageReactionUpdated {
+    fn from(remote: Remote) -> Self {
+        Self {
+            // TODO: #[remote(into)]
+            chat: remote.chat.into(),
+            // TODO: #[remote(into)]
+            message_id: remote.message_id.into(),
+            date: remote.date,
+            // TODO: #[remote(into)]
+            old_reaction: remote
+                .old_reaction
+                .iter()
+                .map(|inner| inner.to_owned().into())
+                .collect(),
+            // TODO: #[remote(into)]
+            new_reaction: remote
+                .new_reaction
+                .iter()
+                .map(|inner| inner.to_owned().into())
+                .collect(),
+            // TODO: #[remote(into)]
+            user: remote.user.map(|inner| inner.into()),
+            // TODO: #[remote(into)]
+            actor_chat: remote.actor_chat.map(|inner| inner.into()),
+        }
+    }
 }
