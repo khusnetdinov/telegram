c9ff96c921e17b7b88973b189d295df4a41ded2f
Marat Khusnetdinov <marat@khusnetdinov.ru>
2024-07-14 19:23:16 +0300

    Remote: Options

--- crates/framework/src/structs/options.rs
+++ crates/framework/src/structs/options.rs
@@ -1,13 +1,25 @@
+use serde::{Deserialize, Serialize};
 use telegram_bots_api::api::enums::reply_markup::ReplyMarkup;
 use telegram_bots_api::api::structs::reply_parameters::ReplyParameters;
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Serialize, Deserialize)]
 pub struct Options {
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub message_thread_id: Option<i64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub disable_notification: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub protect_content: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub reply_parameters: Option<ReplyParameters>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub reply_markup: Option<ReplyMarkup>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub business_connection_id: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub message_effect_id: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub caption: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub show_caption_above_media: Option<bool>,
 }
